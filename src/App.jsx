b3e465df3674f0768fb96a7305403ffa10b1dc4c
moisesnks <mleiva@utem.cl>
2023-12-26 10:49:29 -0300

    Update CSS and JSX files

--- src/App.jsx
+++ src/App.jsx
@@ -372,7 +372,7 @@ function App() {
                           <span className='d-flex my-auto'>{skill.logo}</span>
                           <span className='fw-bold'>{skill.nombre}</span>
                         </div>
-                        <div className="d-flex align-items-center gap-1 barra">
+                        <div className="d-flex align-items-center gap-1 barrita">
                           <div className="barra-container">
                             <div
                               className="barra"
